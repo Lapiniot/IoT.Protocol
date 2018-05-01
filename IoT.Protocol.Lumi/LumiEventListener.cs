c4a7faf63ad246f7b0a0aed8f4eeb1c71e5cb212
Lapiniot <aossss@gmail.com>
2018-05-01 21:42:34 +0300

    Lumi event listener converted to IObservable pattern

--- IoT.Protocol.Lumi/LumiEventListener.cs
+++ IoT.Protocol.Lumi/LumiEventListener.cs
@@ -1,41 +1,41 @@
-﻿using System.Json;
+﻿using System;
+using System.Common;
+using System.Json;
 using System.Net;
-using IoT.Protocol.Lumi.Interfaces;
 using IoT.Protocol.Net.Udp;
+using ILumiObserver = System.IObserver<(string Command, string Sid, System.Json.JsonObject Data, System.Json.JsonObject Message)>;
 
 namespace IoT.Protocol.Lumi
 {
-    public class LumiEventListener : UdpMessageListener
+    public class LumiEventListener : UdpMessageListener, IObservable<(string Command, string Sid, JsonObject Data, JsonObject Message)>
     {
-        private readonly ILumiEventListener listener;
-
-        public LumiEventListener(ILumiEventListener eventListener, IPEndPoint groupEndpoint) : base(groupEndpoint)
+        private ObservableContainer<(string, string, JsonObject, JsonObject)> container;
+        public LumiEventListener(IPEndPoint groupEndpoint) : base(groupEndpoint)
         {
-            listener = eventListener;
+            container = new ObservableContainer<(string, string, JsonObject, JsonObject)>();
+        }
+
+        public IDisposable Subscribe(ILumiObserver observer)
+        {
+            return container.Subscribe(observer);
         }
 
         protected override void OnDataAvailable(IPEndPoint remoteEndPoint, byte[] bytes)
         {
-            var message = (JsonObject) JsonExtensions.Deserialize(bytes);
+            var message = (JsonObject)JsonExtensions.Deserialize(bytes);
 
-            if(message.TryGetValue("sid", out var sid) &&
-               message.TryGetValue("data", out var v) &&
-               JsonValue.Parse(v) is JsonObject data)
+            if(message.TryGetValue("sid", out var sid) && message.TryGetValue("cmd", out var cmd) &&
+                message.TryGetValue("data", out var v) && JsonValue.Parse(v) is JsonObject data)
             {
-                switch((string) message["cmd"])
-                {
-                    case "report":
-                    {
-                        listener.OnReportMessage(sid, data, message);
-                        break;
-                    }
-                    case "heartbeat":
-                    {
-                        listener.OnHeartbeatMessage(sid, data, message);
-                        break;
-                    }
-                }
+                container.Notify((cmd, sid, data, message));
             }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            
+            if(disposing) container.Dispose();
         }
     }
 }
