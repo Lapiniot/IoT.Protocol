4017d84bb8638d31c71a2fd08294cb756328eb2f
Lapiniot <aossss@gmail.com>
2018-05-01 14:39:49 +0300

    TcpMessenger separated

--- IoT.Protocol.Yeelight/YeelightControlEndpoint.cs
+++ IoT.Protocol.Yeelight/YeelightControlEndpoint.cs
@@ -2,11 +2,10 @@
 using System.IO;
 using System.Json;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using IoT.Protocol.Net;
+using IoT.Protocol.Net.Tcp;
 
 namespace IoT.Protocol.Yeelight
 {
@@ -61,50 +60,11 @@ namespace IoT.Protocol.Yeelight
             return false;
         }
 
-
         #region Overrides of DispatchingMessenger<byte[],string>
 
         protected override INetMessenger<byte[], string> CreateNetMessenger()
         {
             return new TcpMessenger(Endpoint);
-        }
-
-        public class TcpMessenger : INetMessenger<byte[], string>
-        {
-            private readonly TcpClient client;
-            private readonly IPEndPoint endpoint;
-            private readonly StreamReader reader;
-            private readonly NetworkStream stream;
-
-            public TcpMessenger(IPEndPoint endpoint)
-            {
-                this.endpoint = endpoint;
-                client = new TcpClient();
-                client.Connect(endpoint);
-                stream = client.GetStream();
-                reader = new StreamReader(stream, Encoding.UTF8, false, 2 * 1024, true);
-            }
-
-            #region Implementation of IDisposable
-
-            public async Task<(IPEndPoint RemoteEP, string Message)> ReceiveAsync(CancellationToken cancellationToken)
-            {
-                return (endpoint, await reader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false));
-            }
-
-            public Task SendAsync(byte[] message, CancellationToken cancellationToken)
-            {
-                return stream.WriteAsync(message, 0, message.Length, cancellationToken);
-            }
-
-            public void Dispose()
-            {
-                reader.Dispose();
-                stream.Dispose();
-                client.Dispose();
-            }
-
-            #endregion
         }
 
         #endregion
