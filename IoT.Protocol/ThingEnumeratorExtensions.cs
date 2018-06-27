4991ba43e78c62ee0c7b2e350779a1beecf0866a
Lapiniot <aossss@gmail.com>
2018-06-28 00:05:27 +0300

    ContentDirectory browsing template added

--- IoT.Protocol/ThingEnumeratorExtensions.cs
+++ IoT.Protocol/ThingEnumeratorExtensions.cs
@@ -15,9 +15,27 @@ namespace IoT.Protocol
             }
         }
 
+        public static IEnumerable<TThing> Enumerate<TThing>(this IThingEnumerator<TThing> enumerator, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            using(var timeoutTokenSource = new CancellationTokenSource(timeout))
+            using(var cts = CancellationTokenSource.CreateLinkedTokenSource(timeoutTokenSource.Token, cancellationToken))
+            {
+                foreach(var thing in enumerator.Enumerate(cts.Token)) yield return thing;
+            }
+        }
+
         public static IEnumerable<TThing> Enumerate<TThing>(this IThingEnumerator<TThing> enumerator, int timeoutMilliseconds)
         {
             using(var cts = new CancellationTokenSource(timeoutMilliseconds))
+            {
+                foreach(var thing in enumerator.Enumerate(cts.Token)) yield return thing;
+            }
+        }
+
+        public static IEnumerable<TThing> Enumerate<TThing>(this IThingEnumerator<TThing> enumerator, int timeoutMilliseconds, CancellationToken cancellationToken)
+        {
+            using(var timeoutTokenSource = new CancellationTokenSource(timeoutMilliseconds))
+            using(var cts = CancellationTokenSource.CreateLinkedTokenSource(timeoutTokenSource.Token, cancellationToken))
             {
                 foreach(var thing in enumerator.Enumerate(cts.Token)) yield return thing;
             }
