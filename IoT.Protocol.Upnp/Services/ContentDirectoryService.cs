43bd06445c73198ff0aaec6317ef23729ec5d357
Lapiniot <aossss@gmail.com>
2018-06-29 00:24:51 +0300

    ContentDirectory browsing

--- IoT.Protocol.Upnp/Services/ContentDirectoryService.cs
+++ IoT.Protocol.Upnp/Services/ContentDirectoryService.cs
@@ -2,13 +2,22 @@
 using System.Threading;
 using System.Threading.Tasks;
 using IoT.Protocol.Soap;
+using static IoT.Protocol.Upnp.UpnpServices;
 
 namespace IoT.Protocol.Upnp.Services
 {
-    public sealed class ContentDirectoryService : SoapActionInvoker
+    public sealed class ContentDirectoryService : ServiceContext
     {
         public ContentDirectoryService(SoapControlEndpoint endpoint, string deviceId) :
-            base(endpoint, new Uri($"{deviceId}-MS/upnp.org-ContentDirectory-1/control", UriKind.Relative), UpnpServices.ContentDirectory)
+            base(endpoint, new Uri($"{deviceId}-MS/upnp.org-ContentDirectory-1/control", UriKind.Relative), ContentDirectory)
+        {
+        }
+
+        public ContentDirectoryService(SoapControlEndpoint endpoint, Uri controlUri) : base(endpoint, controlUri, ContentDirectory)
+        {
+        }
+
+        public ContentDirectoryService(SoapControlEndpoint endpoint) : base(endpoint, ContentDirectory)
         {
         }
 
@@ -16,9 +25,10 @@ namespace IoT.Protocol.Upnp.Services
             string sortCriteria = null, CancellationToken cancellationToken = default)
         {
             return (await InvokeAsync("Browse", cancellationToken,
-                ("ObjectID", parent), ("BrowseFlag", "BrowseDirectChildren"),
-                ("Filter", filter ?? "*"), ("StartingIndex", index),
-                ("RequestedCount", count), ("SortCriteria", sortCriteria ?? "")).ConfigureAwait(false))["Result"];
+                    ("ObjectID", parent), ("BrowseFlag", "BrowseDirectChildren"),
+                    ("Filter", filter ?? "*"), ("StartingIndex", index),
+                    ("RequestedCount", count), ("SortCriteria", sortCriteria ?? "")).
+                ConfigureAwait(false))["Result"];
         }
     }
 }
---
+++ IoT.Protocol.Upnp/Services/ServiceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using IoT.Protocol.Soap;
+
+namespace IoT.Protocol.Upnp.Services
+{
+    public abstract class ServiceContext : SoapActionInvoker
+    {
+        protected ServiceContext(SoapControlEndpoint endpoint, string schema) : base(endpoint, new Uri(".", UriKind.Relative), schema)
+        {
+        }
+
+        protected ServiceContext(SoapControlEndpoint endpoint, Uri controlUri, string schema) : base(endpoint, controlUri, schema)
+        {
+        }
+    }
+}
