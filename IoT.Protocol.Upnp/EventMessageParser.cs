cc0d98797ba13005c082de93b0b4f3079bcabfc5
Lapiniot <aossss@gmail.com>
2020-10-09 21:16:56 +0300

    wrong namespace validation logic bug has been fixed

--- IoT.Protocol.Upnp/EventMessageParser.cs
+++ IoT.Protocol.Upnp/EventMessageParser.cs
@@ -11,7 +11,6 @@ namespace IoT.Protocol.Upnp
     public static class EventMessageParser
     {
         private const string EventNS = "urn:schemas-upnp-org:event-1-0";
-        private const string MetadataNS = "urn:schemas-upnp-org:metadata-1-0/AVT/";
 
         public static async Task<string> ReadLastChangeContentAsync(XmlReader reader)
         {
@@ -36,46 +35,51 @@ namespace IoT.Protocol.Upnp
             return null;
         }
 
-        public static async Task<(IReadOnlyDictionary<string, string> Metadata, IReadOnlyDictionary<string, string> Vendor)> ParseAsync(XmlReader reader)
+        public static async Task<(string Namespace, IReadOnlyDictionary<string, string> Metadata, IReadOnlyDictionary<string, string> Vendor)> ParseAsync(XmlReader reader)
         {
             var content = await ReadLastChangeContentAsync(reader).ConfigureAwait(false);
 
             if(string.IsNullOrEmpty(content)) return default;
 
+            string eventNamesapce = null;
             var metadata = new Dictionary<string, string>();
             var vendor = new Dictionary<string, string>();
 
-            using var ir = XmlReader.Create(new StringReader(content),
+            using var xr = XmlReader.Create(new StringReader(content),
                 new XmlReaderSettings
                 {
                     CloseInput = true, ConformanceLevel = ConformanceLevel.Fragment,
                     IgnoreProcessingInstructions = true, IgnoreWhitespace = true, IgnoreComments = true
                 });
 
-            while(ir.Read() && ir.Depth == 0)
+            while(xr.Read() && xr.Depth == 0)
             {
-                if(ir.NodeType != Element || ir.LocalName != "Event" || ir.NamespaceURI != MetadataNS) continue;
+                if(xr.NodeType != Element || xr.LocalName != "Event") continue;
 
-                while(ir.Read() && ir.Depth == 1)
+                eventNamesapce = xr.NamespaceURI;
+
+                while(xr.Read() && xr.Depth == 1)
                 {
-                    if(ir.NodeType != Element && ir.LocalName != "InstanceID" || ir.NamespaceURI != MetadataNS) continue;
+                    if(xr.NodeType != Element && xr.LocalName != "InstanceID" || xr.NamespaceURI != eventNamesapce) continue;
 
-                    while(ir.Read() && ir.Depth == 2)
+                    while(xr.Read() && xr.Depth == 2)
                     {
-                        if(ir.NodeType != Element) continue;
-                        if(ir.NamespaceURI == MetadataNS)
+                        if(xr.NodeType != Element) continue;
+                        if(xr.NamespaceURI == eventNamesapce)
                         {
-                            metadata[ir.LocalName] = ir.MoveToAttribute("val") ? ir.ReadContentAsString() : ir.ReadElementContentAsString();
+                            metadata[xr.LocalName] = xr.MoveToAttribute("val") ? xr.ReadContentAsString() : xr.ReadElementContentAsString();
                         }
                         else
                         {
-                            vendor[ir.Name] = ir.MoveToAttribute("val") ? ir.ReadContentAsString() : ir.ReadElementContentAsString();
+                            vendor[xr.Name] = xr.MoveToAttribute("val") ? xr.ReadContentAsString() : xr.ReadElementContentAsString();
                         }
                     }
                 }
+
+                break;
             }
 
-            return (metadata, vendor);
+            return (eventNamesapce, metadata, vendor);
         }
     }
 }
