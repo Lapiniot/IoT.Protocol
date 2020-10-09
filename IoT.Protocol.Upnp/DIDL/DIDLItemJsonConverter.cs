d63abd2d3e614c736c63fb94f80c21414bc3a939
Lapiniot <aossss@gmail.com>
2020-10-10 00:29:40 +0300

    ItemJsonConverter renamed to DIDLItemJsonConverter + flexibility improvements

--- IoT.Protocol.Upnp/DIDL/DIDLItemJsonConverter.cs
+++ IoT.Protocol.Upnp/DIDL/DIDLItemJsonConverter.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 
 namespace IoT.Protocol.Upnp.DIDL
 {
-    public class ItemJsonConverter : JsonConverter<Item>
+    public class DIDLItemJsonConverter : JsonConverter<Item>
     {
         public override Item Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
@@ -13,13 +13,15 @@ namespace IoT.Protocol.Upnp.DIDL
 
         public override void Write(Utf8JsonWriter writer, Item value, JsonSerializerOptions options)
         {
+            if(options is null) throw new ArgumentNullException(nameof(options));
+
             switch(value)
             {
                 case Container container:
-                    JsonSerializer.Serialize(writer, container, options);
+                    ((JsonConverter<Container>)options.GetConverter(typeof(Container))).Write(writer, container, options);
                     break;
                 case MediaItem mediaItem:
-                    JsonSerializer.Serialize(writer, mediaItem, options);
+                    ((JsonConverter<MediaItem>)options.GetConverter(typeof(MediaItem))).Write(writer, mediaItem, options);
                     break;
                 default:
                     throw new NotSupportedException("Unsupported type");
