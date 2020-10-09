7ad47fedeb3d547680121adb112b91c155f8680f
Lapiniot <aossss@gmail.com>
2020-10-10 01:42:14 +0300

    audio volume control methods added to RenderingControlService

--- IoT.Protocol.Upnp/Services/RenderingControlService.cs
+++ IoT.Protocol.Upnp/Services/RenderingControlService.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using IoT.Protocol.Soap;
 using static IoT.Protocol.Upnp.UpnpServices;
 
@@ -12,5 +15,30 @@ namespace IoT.Protocol.Upnp.Services
 
         public RenderingControlService(SoapControlEndpoint endpoint) :
             base(endpoint, RenderingControl) {}
+
+        public Task<IDictionary<string, string>> GetVolumeAsync(uint instanceId, CancellationToken cancellationToken = default)
+        {
+            return InvokeAsync("GetVolume", cancellationToken, ("InstanceID", instanceId), ("Channel", "Master"));
+        }
+
+        public Task<IDictionary<string, string>> SetVolumeAsync(uint instanceId, uint volume, CancellationToken cancellationToken = default)
+        {
+            return InvokeAsync("SetVolume", cancellationToken, ("InstanceID", instanceId), ("Channel", "Master"), ("DesiredVolume", volume));
+        }
+
+        public Task<IDictionary<string, string>> GetMuteAsync(uint instanceId, CancellationToken cancellationToken = default)
+        {
+            return InvokeAsync("GetMute", cancellationToken, ("InstanceID", instanceId), ("Channel", "Master"));
+        }
+
+        public Task<IDictionary<string, string>> SetMuteAsync(uint instanceId, bool mute, CancellationToken cancellationToken = default)
+        {
+            return InvokeAsync("SetMute", cancellationToken, ("InstanceID", instanceId), ("Channel", "Master"), ("DesiredMute", mute));
+        }
+
+        public Task<IDictionary<string, string>> GetPresetsAsync(uint instanceId, CancellationToken cancellationToken = default)
+        {
+            return InvokeAsync("ListPresets", cancellationToken, ("InstanceID", instanceId));
+        }
     }
 }
