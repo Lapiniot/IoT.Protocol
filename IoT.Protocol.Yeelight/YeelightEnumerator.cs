a8159f7c245c1eef5eb7dd0c368c5196b635bc77
Lapiniot <aossss@gmail.com>
2022-01-01 13:18:36 +0200

    compatibility fix after submodule update

--- IoT.Protocol.Yeelight/YeelightEnumerator.cs
+++ IoT.Protocol.Yeelight/YeelightEnumerator.cs
@@ -6,6 +6,6 @@ namespace IoT.Protocol.Yeelight;
 public class YeelightEnumerator : SsdpSearchEnumerator
 {
     public YeelightEnumerator(IRepeatPolicy discoveryPolicy) :
-        base("wifi_bulb", SocketBuilderExtensions.GetIPv4MulticastGroup(1982), discoveryPolicy)
+        base("wifi_bulb", SocketBuilderExtensions.GetIPv4MulticastGroup(1982), null, discoveryPolicy)
     { }
 }
