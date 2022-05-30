12719c259cc98176bac8d17cea40437f2955845e
Lapiniot <aossss@gmail.com>
2022-05-30 12:02:15 +0300

    Variance fix to IUpnpServiceFactory<out T>

--- IoT.Protocol.Upnp/IUpnpServiceFactory.cs
+++ IoT.Protocol.Upnp/IUpnpServiceFactory.cs
@@ -2,7 +2,7 @@ using IoT.Protocol.Soap;
 
 namespace IoT.Protocol.Upnp;
 
-public interface IUpnpServiceFactory<T> where T : SoapActionInvoker, IUpnpServiceFactory<T>
+public interface IUpnpServiceFactory<out T> where T : SoapActionInvoker, IUpnpServiceFactory<T>
 {
     static abstract T Create(SoapControlEndpoint endpoint, Uri controlUri);
 }
